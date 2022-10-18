18fb7cfe796efc860c9f8e8a7c21ce13566aa93b
Angelo Vizcaino <ps3ajv@hotmail.com>
2022-10-17 17:05:23 -0700

    Icon Update

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { RefreshIcon } from '@heroicons/react/outline'
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
 import Tweetbox from './Tweetbox'
 import { Tweet } from '../typings'
 import TweetComponent from './Tweet'
@@ -28,7 +28,7 @@ function Feed({ tweets: tweetsProp }: Props) {
     <div className="col-span-7 max-h-screen overflow-scroll border scrollbar-hide lg:col-span-5">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-        <RefreshIcon
+        <ArrowPathIcon
           onClick={HandleRefresh}
           className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
         />
